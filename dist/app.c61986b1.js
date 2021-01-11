a798e3c1c3e449c889544009e268832f4a1a0818
Akram Khalid <akramkhalid12@gmail.com>
2021-01-11 11:22:45 -0600

    Removed animation ts file

--- dist/app.c61986b1.js
+++ dist/app.c61986b1.js
@@ -7468,7 +7468,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56538" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61403" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
